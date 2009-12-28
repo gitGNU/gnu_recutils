64c2cb8218064a397659599f33c56dda93490981
Jose E. Marchesi <jemarch@gnu.org>
2009-12-28 09:23:53 +0100

    foo

---
+++ utils/recinf.c
@@ -0,0 +1,108 @@
+/* -*- mode: C -*- Time-stamp: "09/12/28 09:18:23 jemarch"
+ *
+ *       File:         recinf.c
+ *       Date:         Mon Dec 28 08:54:38 2009
+ *
+ *       GNU Rec - recinf
+ *
+ */
+
+#include <config.h>
+
+#include <getopt.h>
+#include <string.h>
+#include <rec.h>
+
+#include <recinf.h>
+
+/*
+ * Global variables
+ */
+
+char *program_name; /* Initialized in main() */
+
+/*
+ * Command line options management
+ */
+
+static const struct option GNU_longOptions[] =
+  {
+    {"help", no_argument, NULL, HELP_ARG},
+    {"usage", no_argument, NULL, USAGE_ARG},
+    {"version", no_argument, NULL, VERSION_ARG},
+    {"all", no_argument, NULL, PRINTALL_ARG},
+    {NULL, 0, NULL, 0}
+  };
+
+/* Messages */
+
+char *recinf_version_msg = "recinf 1.0";
+
+char *recinf_usage_msg = "\
+Usage: recinf [OPTION]... [FILE]...\n\
+Print information about the specified rec files.\n\
+\n\
+available options\n\
+  -a, --all                           print all available information.\n\
+  --help                              print a help message and exit.\n\
+  --usage                             print a usage message and exit.\n\
+  --version                           show recinf version and exit.\n\
+";
+
+char *recinf_help_msg = "";
+
+int
+main (int argc, char *argv[])
+{
+  char c;
+  bool finish;
+  bool printall;
+  int ret;
+
+  finish = false;
+  printall = false;
+
+  while (!finish &&
+         (ret = getopt_long (argc,
+                             argv,
+                             "a",
+                             GNU_longOptions,
+                             NULL)) != -1)
+    {
+      c = ret;
+      switch (c)
+        {
+          /* COMMON ARGUMENTS */
+        case HELP_ARG:
+          {
+            fprintf (stdout, "%s\n", recinf_usage_msg);
+            exit (0);
+            break;
+          }
+        case VERSION_ARG:
+          {
+            fprintf (stdout, "%s\n", recinf_version_msg);
+            exit (0);
+            break;
+          }
+        case USAGE_ARG:
+          {
+            fprintf (stdout, "%s\n", recinf_usage_msg);
+            exit (0);
+            break;
+          }
+        case PRINTALL_ARG:
+          {
+            printall = true;
+            break;
+          }
+        }
+    }
+
+  program_name = strdup (argv[0]);
+
+
+  return 0;
+}
+
+/* End of recinf.c */
---
+++ utils/recinf.h
@@ -0,0 +1,26 @@
+/* -*- mode: C -*- Time-stamp: "09/12/28 08:58:46 jemarch"
+ *
+ *       File:         recinf.h
+ *       Date:         Mon Dec 28 08:54:38 2009
+ *
+ *       GNU Rec - recinf
+ *
+ */
+
+#ifndef RECINF_H
+#define RECINF_H
+
+/* Command line arguments */
+
+enum
+{
+  HELP_ARG,
+  USAGE_ARG,
+  VERSION_ARG,
+  PRINTALL_ARG
+};
+
+
+#endif /* recinf.h */
+
+/* End of recinf.h */
