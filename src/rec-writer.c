45bf565d5ca18af71a1742af004338469c06e4d1
Jose E. Marchesi <jemarch@gnu.org>
2009-12-28 09:35:43 +0100

    foo

---
+++ src/rec-writer.c
@@ -0,0 +1,219 @@
+/* -*- mode: C -*- Time-stamp: "09/12/28 08:42:01 jemarch"
+ *
+ *       File:         rec-writer.c
+ *       Date:         Sat Dec 26 22:47:16 2009
+ *
+ *       GNU Rec library - Writer
+ *
+ */
+
+/* Copyright (C) 2009 Jose E. Marchesi */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <rec.h>
+
+#include <malloc.h>
+#include <string.h>
+
+#include <rec.h>
+
+/*
+ * Static functions defined in this file
+ */
+static bool rec_writer_putc (rec_writer_t writer, char c);
+static bool rec_writer_puts (rec_writer_t writer, const char *s);
+
+/* Writer Data Structure
+ *
+ */
+struct rec_writer_s
+{
+  FILE *out; /* File stream used by the writer. */
+
+  bool eof;
+  int line;  /* Current line number. */
+};
+
+rec_writer_t
+rec_writer_new (FILE *out)
+{
+  rec_writer_t new;
+
+  new = malloc (sizeof(struct rec_writer_s));
+  if (new)
+    {
+      new->out = out;
+      new->line = 1;
+      new->eof = false;
+    }
+
+  return new;
+}
+
+void
+rec_writer_destroy (rec_writer_t writer)
+{
+  free (writer);
+}
+
+bool
+rec_write_field (rec_writer_t writer,
+                 rec_field_t field)
+{
+  int i;
+  size_t pos;
+  rec_field_name_t fname;
+  const char *fvalue;
+
+  /* Write the field name */
+  fname = rec_field_name (field);
+  for (i = 0; i < rec_field_name_size (fname); i++)
+    {
+      if (!rec_writer_puts (writer,
+                            rec_field_name_get (fname, i)))
+        {
+          return false;
+        }
+      if (!rec_writer_putc (writer, ':'))
+        {
+          return false;
+        }
+    }
+
+  if (!rec_writer_putc (writer, ' '))
+    {
+      return false;
+    }
+  
+
+  /* Write the field value */
+  fvalue = rec_field_value (field);
+  for (pos = 0; pos < strlen (fvalue); pos++)
+    {
+      if (fvalue[pos] == '\n')
+        {
+          if (!rec_writer_puts (writer, "\n+ "))
+            {
+              /* EOF on output */
+              return false;
+            }
+        }
+      else
+        {
+          if (!rec_writer_putc (writer, fvalue[pos]))
+            {
+              /* EOF on output */
+              return false;
+            }
+        }
+    }
+
+  if (!rec_writer_putc (writer, '\n'))
+    {
+      /* EOF on output */
+      return false;
+    }
+  
+  return true;
+}
+
+bool
+rec_write_record (rec_writer_t writer,
+                  rec_record_t record)
+{
+  bool ret;
+  rec_field_t field;
+  int i;
+
+  ret = true;
+  for (i = 0; i < rec_record_size (record); i++)
+    {
+      field = rec_record_get_field (record, i);
+      if (!rec_write_field (writer, field))
+        {
+          ret = false;
+          break;
+        }
+    }
+
+  return ret;
+}
+
+bool
+rec_write_rset (rec_writer_t writer,
+                rec_rset_t rset)
+{
+  int i;
+  bool ret;
+  rec_record_t descriptor;
+  bool wrote_descriptor;
+
+  ret = true;
+  wrote_descriptor = false;
+
+  descriptor = rec_rset_descriptor (rset);
+  if (descriptor
+      && (!(wrote_descriptor = rec_write_record (writer, rec_rset_descriptor (rset)))))
+    {
+      ret = false;
+    }
+  else
+    {
+      if (wrote_descriptor)
+        {
+          if (!rec_writer_putc (writer, '\n'))
+
+            {
+              ret = false;
+            }
+        }
+
+      for (i = 0; i < rec_rset_size (rset); i++)
+        {
+          if (!rec_write_record (writer,
+                                 rec_rset_get_record (rset, i)))
+            {
+              ret = false;
+            }
+
+          if (!rec_writer_putc (writer, '\n'))
+
+            {
+              ret = false;
+            }
+        }
+    }
+
+  return ret;
+}
+
+/*
+ * Private functions
+ */
+
+static bool
+rec_writer_putc (rec_writer_t writer, char c)
+{
+  return (fputc (c, writer->out) != EOF);
+}
+
+static bool
+rec_writer_puts (rec_writer_t writer, const char *s)
+{
+  return (fputs (s, writer->out) != EOF);
+}
+
+/* End of rec-writer.c */
