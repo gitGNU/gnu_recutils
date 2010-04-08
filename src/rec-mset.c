59d60c771a64f6264c6b45f377396161ca505b04
Jose E. Marchesi <jemarch@gnu.org>
2010-04-08 18:27:10 +0200

    Add distribution terms to rec-mset

--- src/rec-mset.c
+++ src/rec-mset.c
@@ -1,10 +1,26 @@
-/* -*- mode: C -*- Time-stamp: "2010-04-08 16:05:08 jemarch"
+/* -*- mode: C -*- Time-stamp: "2010-04-08 18:26:14 jemarch"
  *
  *       File:         rec-mset.c
  *       Date:         Thu Apr  1 17:07:00 2010
  *
  *       GNU recutils - Ordered Heterogeneous Set
  *
+ */
+
+/* Copyright (C) 2010 Jose E. Marchesi */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <config.h>
