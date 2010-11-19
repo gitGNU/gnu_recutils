0a9ae763a30da866e47f73a25b6f2c68c34fd874
Jose E. Marchesi <jemarch@gnu.org>
2010-11-19 19:16:31 +0100

    Tasktool operations.

--- torture/utils/recsel.sh
+++ torture/utils/recsel.sh
@@ -50,6 +50,20 @@ field2: value32
 field3: value33
 '
 
+test_declare_input_file multiline \
+'field1: foo bar \
+baz
+field2: foo
++ bar
++ baz
+
+field1: jo ja \
+ju
+field2:
++ foo
++ bar
+'
+
 #
 # Declare tests
 #
@@ -97,6 +111,124 @@ field3: value23
 
 field2: value32
 field3: value33
+'
+
+# Print values
+test_tool recsel-values \
+          recsel \
+          '-P field2' \
+          multiple-records \
+'value12
+
+value22
+
+value32
+'
+
+# Print multiple values
+test_tool recsel-multiple-values \
+          recsel \
+          '-P field1,field3' \
+          multiple-records \
+'value11
+value13
+
+value21
+value23
+
+value31
+value33
+'
+
+# Print collapsed
+test_tool recsel-collapsed \
+          recsel \
+          '-C -p field1' \
+          multiple-records \
+'field1: value11
+field1: value21
+field1: value31
+'
+
+# Print values collapsed
+test_tool recsel-values-collapsed \
+          recsel \
+          '-C -P field2,field3' \
+          multiple-records \
+'value12
+value13
+value22
+value23
+value32
+value33
+'
+
+# Print in a row
+test_tool recsel-row \
+          recsel \
+          '-R field1,field2' \
+          multiple-records \
+'value11 value12
+
+value21 value22
+
+value31 value32
+'
+
+# Print collapsed in a row
+test_tool recsel-collapsed-row \
+          recsel \
+          '-C -R field1,field2,field3' \
+          multiple-records \
+'value11 value12 value13
+value21 value22 value23
+value31 value32 value33
+'
+
+# Print all fields (multiline)
+test_tool recsel-multi-all-fields \
+          recsel \
+          '' \
+          multiline \
+'field1: foo bar baz
+field2: foo
++ bar
++ baz
+
+field1: jo ja ju
+field2: 
++ foo
++ bar
+'
+
+# Print values (multiline)
+test_tool recsel-multi-values \
+          recsel \
+          '-P field1,field2' \
+          multiline \
+'foo bar baz
+foo
+bar
+baz
+
+jo ja ju
+
+foo
+bar
+'
+# Print collapsed values (multiline)
+test_tool recsel-multi-collapsed-values \
+          recsel \
+          '-C -P field1,field2' \
+          multiline \
+'foo bar baz
+foo
+bar
+baz
+jo ja ju
+
+foo
+bar
 '
 
 #
