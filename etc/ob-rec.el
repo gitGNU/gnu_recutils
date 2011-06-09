a4d655958d12e769e3d008d169bc1a474007c3d6
Jose E. Marchesi <jemarch@gnu.org>
2011-06-09 23:12:32 +0200

    ob-rec.el is not part of Emacs.

--- etc/ob-rec.el
+++ etc/ob-rec.el
@@ -7,7 +7,7 @@
 ;; Homepage: http://orgmode.org
 ;; Version: 7.4
 
-;; This file is part of GNU Emacs.
+;; This file is NOT part of GNU Emacs.
 
 ;; This program is free software: you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
