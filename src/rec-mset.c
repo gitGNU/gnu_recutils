faa15bb3ddbb83c17c7576e0107020b34935226f
Jose E. Marchesi <jemarch@gnu.org>
2012-01-02 22:28:16 +0100

    mset: disposal callback is now actually used.  Memory leak fixed.

--- src/rec-mset.c
+++ src/rec-mset.c
@@ -815,6 +815,15 @@ rec_mset_elem_new (rec_mset_t mset,
 static void
 rec_mset_elem_destroy (rec_mset_elem_t elem)
 {
+  /* Dispose the data stored in the element if a disposal callback
+     function was configured by the user.  The callback is never
+     invoked if the stored data is NULL.  */
+
+  if (elem->data && elem->mset->disp_fn[elem->type])
+    {
+      elem->mset->disp_fn[elem->type] (elem->data);
+    }
+
   free (elem);
 }
 
