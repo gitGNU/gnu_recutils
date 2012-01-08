5daedf1f53a643bce9e588eb2d17f837382f4946
Jose E. Marchesi <jemarch@gnu.org>
2012-01-08 03:38:19 +0100

    Copyright years update.

--- utils/recutl.c
+++ utils/recutl.c
@@ -7,7 +7,7 @@
  *
  */
 
-/* Copyright (C) 2010, 2011 Jose E. Marchesi */
+/* Copyright (C) 2010, 2011, 2012 Jose E. Marchesi */
 
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -178,7 +178,7 @@ recutl_print_version (void)
 Copyright (C) %s Jose E. Marchesi.\n\
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n\
 This is free software: you are free to change and redistribute it.\n\
-There is NO WARRANTY, to the extent permitted by law.\n"), "2010, 2011");
+There is NO WARRANTY, to the extent permitted by law.\n"), "2010, 2011, 2012");
 
   puts (_("\
 \n\
